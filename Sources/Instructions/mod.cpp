bd332ab5b1a62c98e96fa5cab6f875347ea6864a
EtienneLicheron <etienne.licheron@epitech.eu>
2022-07-15 16:31:39 +0200

    [Instructions] Mod :white_check_mark:

--- Sources/Instructions/mod.cpp
+++ Sources/Instructions/mod.cpp
@@ -10,5 +10,16 @@
 namespace Abstract {
     void Core::mod(std::string value) {
         (void)value;
+        if (_stack.size() < 2)
+            throw Exception("Not enough elements (at least two)");
+        IOperand *second = _stack.back();
+        _stack.pop_back();
+        IOperand *first = _stack.back();
+        _stack.pop_back();
+        if (second->toString() == "0")
+            throw Exception("Division by zero");
+        _stack.push_back(first->operator%(*second));
+        delete(first);
+        delete(second);
     }
 }
