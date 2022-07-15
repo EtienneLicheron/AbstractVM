ea5422387a4ab28c1243f4f1274ed9b2132cc2ea
EtienneLicheron <etienne.licheron@epitech.eu>
2022-07-15 21:50:45 +0200

    [Instrunction] print

--- Sources/Instructions/print.cpp
+++ Sources/Instructions/print.cpp
@@ -10,5 +10,12 @@
 namespace Abstract {
     void Core::print(std::string value) {
         (void)value;
+        char ascii;
+        std::string newValue = value.substr(value.find_first_of('(') + 1);
+        newValue.pop_back();
+        if (_stack.back()->getType() != Int8)
+            throw Exception("Operand must be type of Int8");
+        ascii = std::stoi(_stack.back()->toString());
+        std::cout << ascii << std::endl;
     }
 }
