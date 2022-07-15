d4eae617fae537ef258da4e97e7f00ec2c9b1052
EtienneLicheron <etienne.licheron@epitech.eu>
2022-07-15 12:06:05 +0200

    fixed regex [1/2]

--- Sources/Core.cpp
+++ Sources/Core.cpp
@@ -73,10 +73,11 @@ namespace Abstract {
     {
         std::string line;
         std::regex commentary("^(;.*)$");
-        std::regex commands("(((push|assert|load|store)\\s*((int8|int16|int32)\\([-]?[0-9]+\\)|(float|double|bigdecimal)\\([-]?[0-9]+[.]?[0-9]*\\)))|(pop|dump|clear|dup|swap|add|sub|mul|div|mod|print)\\s*)|exit\\s*");
+        std::regex commands("(((push|assert|load|store)\\s*((int8|int16|int32)\\([-]?[0-9]+\\)|(float|double|bigdecimal)\\([-]?[0-9]+[.]?[0-9]*\\)))|(pop|dump|clear|dup|swap|add|sub|mul|div|mod|print)($|\\s*$|\\s*;.*))|exit\\s*");
 
         while (std::getline(in, line)) {
-            if (std::regex_match(line, commentary))
+            std::cout << line << std::endl;
+            if (std::regex_match(line, commentary) || line.empty())
                 continue;
             else if (std::regex_match(line, commands)) {
                 parseLine(line);
